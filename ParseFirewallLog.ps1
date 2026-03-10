c6d7052557e3bbe96f982673ddd9fa3068c7571e
BetaHydri <Jan_Tiedeamann@hotmail.com>
2026-03-10 15:38:12 +0100

    Update script metadata and improve parameter validation in Get-WindowsFirewallLog function

--- ParseFirewallLog.ps1
+++ ParseFirewallLog.ps1
@@ -13,9 +13,9 @@
 
 .TAGS Firewall, Log, 'Windows Firewall', Parser
 
-.LICENSEURI 
+.LICENSEURI https://github.com/BetaHydri/ParseFirewallLog/blob/master/LICENSE
 
-.PROJECTURI 
+.PROJECTURI https://github.com/BetaHydri/ParseFirewallLog
 
 .ICONURI 
 
@@ -29,28 +29,6 @@
 
 
 #>
-
-<# 
-
-    .SYNOPSIS
-    Reads the pfirewall.log file
-
-    .DESCRIPTION
-    Parses the Windows Firewall logfile to be able to filter for specific firewall log entries
-
-    .EXAMPLE
-    Get-WindowsFirewallLog -LogFile .\pfirewall.log | Where-Object { $_.Protocol -eq "TCP" -and $_.Action -eq "DROP" } | Format-Table -AutoSize
-
-    .EXAMPLE
-    Get-WindowsFirewallLog | Select-Object -Property Action, Protocol, Source, SrcPort, Destination, DstPort, Size, Direction | Group-Object -Property Action, Direction | Format-Table -Autosize
-
-    .INPUTS
-    -LogFile        [<windows firewall logfile>]
-
-    .OUTPUTS
-    PsCustomObject with selected Properties
-
-#> 
 
 #Firewall Log Parsing with RegEx
 Function Get-WindowsFirewallLog {
@@ -76,21 +54,19 @@ Function Get-WindowsFirewallLog {
     [CmdletBinding()]
     Param (
         [Parameter(Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [String[]]
         $LogFile = @("$env:windir\system32\LogFiles\Firewall\pfirewall.log")
     )
     # IPv4 RegEx for Windows Firewall Log.
-    [regex]$regex = '(?<Date>\d{4}-\d{2}-\d{2})\s(?<Time>\d{1,2}:\d{1,2}:\d{1,2})\s(?<Action>\w{4,5})\s(?<Protocol>\w{3})\s(?<Source>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(?<Destination>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(?<SrcPort>\d+)\s(?<DstPort>\d+)\s(?<Size>\d+)\s(?<tcpflags>-|\d+)\s(?<tcpsyn>-|\d+)\s(?<tcpack>-|\d+)\s(?<tcpwin>-|\d+)\s(?<icmptype>-|\d+)\s(?<icmpcode>-|\d+)\s(?<info>-|\d+)\s(?<Direction>RECEIVE|SEND)'
-    #$Matches = $null
-    $myArray = @()
+    [regex]$regex = '(?<Date>\d{4}-\d{2}-\d{2})\s(?<Time>\d{1,2}:\d{1,2}:\d{1,2})\s(?<Action>\w{4,5})\s(?<Protocol>\w{3,4})\s(?<Source>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(?<Destination>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(?<SrcPort>\d+)\s(?<DstPort>\d+)\s(?<Size>\d+)\s(?<tcpflags>-|\d+)\s(?<tcpsyn>-|\d+)\s(?<tcpack>-|\d+)\s(?<tcpwin>-|\d+)\s(?<icmptype>-|\d+)\s(?<icmpcode>-|\d+)\s(?<info>-|\d+)\s(?<Direction>RECEIVE|SEND)'
 
     foreach ($item in (Get-Content -Path $LogFile)) {
         if ($item -match $regex) {
-            $Matches.Remove(0)         
-            $myArray += [PSCustomObject]$Matches
+            $Matches.Remove(0)
+            [PSCustomObject]$Matches
         }
     }
-    return $myArray
 }
 
 #Samples
